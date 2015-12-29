9ec762014c9f970ef045ddc53c14111764454902
Mario Longobardi <mlongobardi2@bloomberg.net>
2015-12-28 21:00:50 -0500

    Add fold-based traversal

--- ControlledVisit.hs
+++ ControlledVisit.hs
@@ -38,3 +38,40 @@ getInfo path = do
     size <- maybeIO (bracket (openFile path ReadMode) hClose hFileSize)
     modified <- maybeIO (getModificationTime path)
     return (Info path perms size modified)
+
+data Iterate seed = Done {unwrap :: seed}
+                  | Skip {unwrap :: seed}
+                  | Continue {unwrap :: seed}
+                    deriving (Show)
+
+type Iterator seed = seed -> Info -> Iterate seed
+
+foldTree :: Iterator a -> a -> FilePath -> IO a
+foldTree iter initSeed path = do
+    endSeed <- fold initSeed path
+    return (unwrap endSeed)
+  where
+    fold seed subpath = getUsefulContents subpath >>= walk seed
+    walk seed (name : names) = do
+        let path' = path </> name
+        info <- getInfo path'
+        case iter seed info of
+            done@(Done _) -> return done
+            Skip seed' -> walk seed' names
+            Continue seed'
+                | isDirectory info -> do
+                    next <- fold seed' path'
+                    case next of
+                        done@(Done _) -> return done
+                        seed'' -> walk (unwrap seed'') names
+                | otherwise -> walk seed' names
+    walk seed _ = return (Continue seed)
+
+atMostThreePictures :: Iterator [FilePath]
+atMostThreePictures paths info
+    | length paths == 3 = Done paths
+    | isDirectory info && takeFileName path == ".svn" = Skip paths
+    | extension `elem` [".jpg", ".png"] = Continue (path : paths)
+    | otherwise = Continue paths
+  where extension = map toLower (takeExtension path)
+        path = infoPath info
